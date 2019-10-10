09617b8e09b5743331bb410dbd033d046139da4b
Dan Nicolici <dannicolici@gmail.com>
2019-10-10 15:04:57 +0300

    move IO to Console

--- src/main/java/ro/bitgloss/Main.java
+++ src/main/java/ro/bitgloss/Main.java
@@ -1,10 +1,6 @@
 package ro.bitgloss;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import ro.bitgloss.dao.AppointmentDAO;
-import ro.bitgloss.data.DataSource;
 import ro.bitgloss.io.Console;
 import ro.bitgloss.managers.AppointmentManager;
 import ro.bitgloss.view.ListView;
@@ -12,36 +8,35 @@ import ro.bitgloss.view.TabularView;
 import ro.bitgloss.view.View;
 
 public class Main {
+  private static final String MENU = "Menu\n" +
+          "l - list view\n" +
+          "t - tabular view\n" +
+          "a - add new appointment\n" +
+          "x - exit";
+
   private static ListView listView = new ListView();
   private static TabularView tabularView = new TabularView();
   private static final AppointmentDAO dao = new AppointmentDAO();
   private static final AppointmentManager appointmentManager = new AppointmentManager(dao);
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-    while (true) {
-      Console.printLine("Menu\nl - list view\nt - tabular view\na - add new appointment\nx - exit");
-      String choice = br.readLine();
-      if (choice.equals("x"))
-        break;
-
+  public static void main(String[] args) {
+    for (char choice = Console.choice(MENU); choice != 'x'; choice = Console.choice(MENU)) {
       switch (choice) {
-        case "l":
-          display(dao, listView);
+        case 'l':
+          display(listView);
           break;
-        case "t":
-          display(dao, tabularView);
+        case 't':
+          display(tabularView);
           break;
-        case "a":
+        case 'a':
           appointmentManager.inputNewAppointment();
           break;
-        default: System.out.println("Invalid choice");
+        default: Console.printLine("Invalid choice");
       }
     }
   }
 
-  private static void display(AppointmentDAO dao, View view) {
+  private static void display(View view) {
     if (dao.appointmentsCount() > 0)
       Console.print(view.display(dao));
     else
--- src/main/java/ro/bitgloss/io/Console.java
+++ src/main/java/ro/bitgloss/io/Console.java
@@ -1,11 +1,27 @@
 package ro.bitgloss.io;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Console {
+    private static BufferedReader SYS_IN_READER = new BufferedReader(new InputStreamReader(System.in));
+
     public static void print(Object o) {
         System.out.print(o);
     }
 
     public static void printLine(Object o) {
         print(o + "\n");
+    }
+
+    public static char choice(String menu) {
+        printLine(menu);
+        try {
+            return SYS_IN_READER.readLine().charAt(0);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return 'x';
+        }
     }
 }
