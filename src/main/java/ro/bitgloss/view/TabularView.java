145eef7394ead7f0282b9553c8bcd916d2f3b609
Dan Nicolici <dan.nicolici@mylaps.com>
2020-09-22 09:09:33 +0300

    simplify tabular view

--- src/main/java/ro/bitgloss/view/TabularView.java
+++ src/main/java/ro/bitgloss/view/TabularView.java
@@ -7,44 +7,41 @@ import java.util.function.Function;
 
 public class TabularView {
 
-  private static final String PAD = "          ";
+    private static final String PAD = "          ";
 
-  public static Function<DataSource, String> format = ds -> {
-    StringBuilder sb = new StringBuilder();
-    String rowSeparator = rowSeparator(ds.entryDetails());
-    sb.append(rowSeparator);
-    sb.append(headers(ds));
-    sb.append(rowSeparator);
-    sb.append(data(ds));
-    sb.append(rowSeparator);
+    public static Function<DataSource, String> format = ds -> {
+        String rowSeparator = rowSeparator(ds.entryDetails());
+        return rowSeparator +
+                headers(ds) +
+                rowSeparator +
+                data(ds) +
+                rowSeparator;
+    };
 
-    return sb.toString();
-  };
+    private static StringBuilder data(DataSource ds) {
+        StringBuilder sb = new StringBuilder();
+        ds.stream().forEach(row -> {
+            row.forEach(d -> sb.append(cell(d)));
+            sb.append("\n");
+        });
+        return sb;
+    }
 
-  private static StringBuilder data(DataSource ds) {
-    StringBuilder sb = new StringBuilder();
-    ds.stream().forEach(row -> {
-      row.forEach(d -> sb.append(cell(d)));
-      sb.append("\n");
-    });
-    return sb;
-  }
+    private static StringBuilder headers(DataSource ds) {
+        StringBuilder sb = new StringBuilder();
+        ds.entryDetails().forEach(d -> sb.append("|").append(cell(d)));
+        sb.append("|\n");
 
-  private static StringBuilder headers(DataSource ds) {
-    StringBuilder sb = new StringBuilder();
-    ds.entryDetails().forEach(d -> sb.append("|").append(cell(d)));
-    sb.append("|\n");
-    
-    return sb;
-  }
+        return sb;
+    }
 
-  private static String cell(String data) {
-    return PAD +data+ PAD;
-  }
+    private static String cell(String data) {
+        return PAD + data + PAD;
+    }
 
-  private static String rowSeparator(List<String> headers) {
-    int headersCharCount = headers.stream().mapToInt(String::length).sum();
-    int rowSize = headersCharCount + PAD.length() * 2 * headers.size() + headers.size() + 1;
-    return "-".repeat(Math.max(0, rowSize)) + "\n";
-  }
+    private static String rowSeparator(List<String> headers) {
+        int headersCharCount = headers.stream().mapToInt(String::length).sum();
+        int rowSize = headersCharCount + PAD.length() * 2 * headers.size() + headers.size() + 1;
+        return "-".repeat(Math.max(0, rowSize)) + "\n";
+    }
 }
